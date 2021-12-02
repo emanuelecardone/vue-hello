d86c85e99e363355d3e4308d5b3e8c6006f9e81b
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-02 14:23:50 +0100

    Fatta parte 2 (rimozione d-none)

--- Bonus - card Bootstrap + input/js/script.js
+++ Bonus - card Bootstrap + input/js/script.js
@@ -7,14 +7,33 @@ const app = new Vue(
             // Stili
             cardClass: 'card',
             cardStyle: 'width: 18rem;',
-            imageSrc: 'img/squidgame1.jfif',
-            imageClass: 'card-img-top',
-            imageAlt: 'Immagine di Squid Game',
+            mainImageSrc: 'img/squidgame1.jfif',
+            mainImageClass: 'card-img-top',
+            mainImageAlt: 'Immagine di Squid Game',
             cardBodyClass: 'card-body',
             cardTitleClass: 'card-title',
             cardTextClass: 'card-text',
             cardBtnHref: '#',
             cardBtnClass: 'btn btn-primary',
+            formClass: 'form_wrapper my-3 d-none',
+            othersClass: 'other_images_main_wrapper w-100 pb-2 d-none',
+            labelFor: 'exampleInputText',
+            labelClass: 'form-label',
+            inputType: 'text',
+            inputClass: 'form-control',
+            inputAriaDescribedBy: 'textHelp',
+            helpClass: 'form-text',
+            fluidClass: 'container-fluid',
+            rowClass: 'row row-cols-3',
+            colClass: 'col',
+            othersWrapperClass: 'other_images_sub_wrapper w-100 h-100',
+            othersImageClass: 'other_image w-100 h-100',
+            secondOtherImageSrc: 'img/squidgame2.jpg',
+            secondOtherImageAlt: 'Immagine di Squid Game 2',
+            thirdOtherImageSrc: 'img/squidgame3.jpg',
+            thirdOtherImageAlt: 'Immagine di Squid Game 3',
+            fourthOtherImageSrc: 'img/squidgame4.jpg',
+            fourthOtherImageAlt: 'Immagine di Squid Game 4',
 
             // Contenuti
             cardTitleContent: 'Squid Game',
@@ -22,7 +41,11 @@ const app = new Vue(
             cardBtnContent: 'Switch image'
         },
         methods: {
-
+            // Rimozione d-none al click di button
+            showFormAndOthers: function(){
+                this.formClass = 'form_wrapper my-3';
+                this.othersClass = 'other_images_main_wrapper w-100 pb-2';
+            }
         }
     }
 );
