091bb3680015fc47ef7dc9bd6860b0b662d492b1
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-02 15:20:52 +0100

    Fatto effetto switch-toggle alle img da input text

--- Bonus - card Bootstrap + input/js/script.js
+++ Bonus - card Bootstrap + input/js/script.js
@@ -40,13 +40,14 @@ const app = new Vue(
             cardTitleContent: 'Squid Game',
             cardTextContent: 'The Squid Game series revolves around a contest where 456 players, all of whom are in deep financial debt, risk their lives to play a series of deadly children\'s games for the chance to win a 45.6$ billion prize.',
             cardBtnContent: 'Switch image',
-            labelContent: 'Type here the name of the image',
-            imagesNames: '(Image 2, Image 3, Image 4)',
-            inputContent: ''
+            labelContent: 'Type here the index of the image',
+            imagesNames: '(Index: 1, 2, 3)',
+            inputContent: '',
+            othersBtnContent: 'Confirm'
         },
 
         methods: {
-            // Rimozione d-none al click di button
+            // Rimozione d-none al click di button (effetto toggle apri-chiudi)
             showFormAndOthers: function(){
                 if(this.cardBtnContent === 'Switch image'){
                     this.formClass = 'form_wrapper my-3';
@@ -58,6 +59,46 @@ const app = new Vue(
                     this.cardBtnContent = 'Switch image';
                 }
                 
+            },
+            switchImage: function(){
+                // Effetto toggle: se la src dell'immagine grande non è quella che corrisponde con l'index che scrivo nella input, lo diventa. 
+                // La foto piccola che corrisponde alla mia scelta assumerà la src che aveva l'immagine grande
+                // Se scelgo Index 1, quella che era grande diventerà Index 1. Se riscrivo Index 1 si riscambiano
+                // Se ho scelto Index 1 e poi scrivo Index 3, la Index 1 diventa main e poi diventa Index 3, la vecchia Index 3 ora sarà main
+                // Questo switch è possibile in qualsiasi momento tenendo sempre conto della posizione attuale dell'immagine che voglio guardare
+                switch(this.inputContent){
+                    case '1':
+                        if(this.mainImageSrc !== 'img/squidgame2.jpg'){
+                            this.secondOtherImageSrc = this.mainImageSrc;
+                            this.mainImageSrc = 'img/squidgame2.jpg';
+                            break;
+                        } else{
+                            this.mainImageSrc = this.secondOtherImageSrc;
+                            this.secondOtherImageSrc = 'img/squidgame2.jpg';
+                            break;
+                        }
+                    case '2':
+                        if(this.mainImageSrc !== 'img/squidgame3.jpg'){
+                            this.thirdOtherImageSrc = this.mainImageSrc;
+                            this.mainImageSrc = 'img/squidgame3.jpg';
+                            break;
+                        } else{
+                            this.mainImageSrc = this.thirdOtherImageSrc;
+                            this.thirdOtherImageSrc = 'img/squidgame3.jpg';
+                            break;
+                        }
+                    case '3':
+                        if(this.mainImageSrc !== 'img/squidgame4.jpg'){
+                            this.fourthOtherImageSrc = this.mainImageSrc;
+                            this.mainImageSrc = 'img/squidgame4.jpg';
+                            break;
+                        } else{
+                            this.mainImageSrc = this.fourthOtherImageSrc;
+                            this.fourthOtherImageSrc = 'img/squidgame4.jpg';
+                            break;
+                        }
+                        
+                }
             }
         }
     }
