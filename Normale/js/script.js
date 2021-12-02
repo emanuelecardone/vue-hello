a22c94cd33fda33e0e07d6e670419b24ec6bc8da
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-02 13:07:45 +0100

    Fatto esercizio normale

---
+++ Normale/js/script.js
@@ -0,0 +1,18 @@
+// Descrizione:
+// Stampare a schermo un messaggio all’interno di un h1, utilizzando i data.
+// BONUS:
+// Aggiungere alla pagina un’immagine, presa anch’essa da un data.
+
+Vue.config.devtools = true;
+
+const app = new Vue(
+    {
+        el: '#root',
+        data:{
+            titleContent: 'Ciao sono un titolo H1',
+            imageSource: 'img/niagara.jpg',
+            imageAlt: 'Cascate del Niagara',
+            imageClass: 'image_size'
+        }
+    }
+);
