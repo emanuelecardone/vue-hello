7909a1d47d95faf3d1fb2d64f18d2d2288057eab
emanuelecardone <e_m.cardone@yahoo.it>
2021-12-02 16:35:39 +0100

    Aggiunto effetto active alle thumbs

--- Bonus - card Bootstrap + input/js/script.js
+++ Bonus - card Bootstrap + input/js/script.js
@@ -9,7 +9,7 @@ const app = new Vue(
             cardClass: 'card',
             cardStyle: 'width: 18rem;',
             mainImageSrc: 'img/squidgame1.jfif',
-            mainImageClass: 'card-img-top',
+            mainImageClass: 'w-100 h_200p',
             mainImageAlt: 'Immagine di Squid Game',
             cardBodyClass: 'card-body',
             cardTitleClass: 'card-title',
@@ -28,7 +28,10 @@ const app = new Vue(
             rowClass: 'row row-cols-4',
             colClass: 'col',
             othersWrapperClass: 'other_images_sub_wrapper w-100 h-100',
-            othersImageClass: 'other_image w-100 h-100',
+            othersFirstClass: 'other_image other_1 active',
+            othersSecondClass: 'other_image other_2',
+            othersThirdClass: 'other_image other_3',
+            othersFourthClass: 'other_image other_4',
             firstOtherImageSrc: 'img/squidgame1.jfif',
             firstOtherImageAlt: 'Immagine di Squid Game 1',
             secondOtherImageSrc: 'img/squidgame2.jpg',
@@ -62,21 +65,38 @@ const app = new Vue(
                 }
                 
             },
-            // Up: Cambiata la funzione, ora viene mostrata come immagine grande quella
-            // corrispondente all'index inserito nell'input dall'utente (da aggiungere effetto active alle thumbs)
+            // Viene mostrata come immagine grande quella corrispondente all'index inserito nell'input dall'utente
+            // La thumb corrispondente avrà un'animazione e la classe active
+            // Inoltre, rimuoverà la classe active a tutte le altre così sono sicuro di rimuovere l'active da qualsiasi thumb sia active
             switchImage: function(){
                 switch(this.inputContent){
                     case '1':
                         this.mainImageSrc = this.firstOtherImageSrc;
+                        this.othersFirstClass = 'other_image other_1 active';
+                        this.othersSecondClass= 'other_image other_2';
+                        this.othersThirdClass= 'other_image other_3';
+                        this.othersFourthClass= 'other_image other_4';
                         break;
                     case '2':
                         this.mainImageSrc = this.secondOtherImageSrc;
+                        this.othersFirstClass = 'other_image other_1';
+                        this.othersSecondClass= 'other_image other_2 active';
+                        this.othersThirdClass= 'other_image other_3';
+                        this.othersFourthClass= 'other_image other_4';
                         break;
                     case '3':
                         this.mainImageSrc = this.thirdOtherImageSrc;
+                        this.othersFirstClass = 'other_image other_1';
+                        this.othersSecondClass= 'other_image other_2';
+                        this.othersThirdClass= 'other_image other_3 active';
+                        this.othersFourthClass= 'other_image other_4';
                         break;
                     case '4':
                         this.mainImageSrc = this.fourthOtherImageSrc;
+                        this.othersFirstClass = 'other_image other_1';
+                        this.othersSecondClass= 'other_image other_2';
+                        this.othersThirdClass= 'other_image other_3';
+                        this.othersFourthClass= 'other_image other_4 active';
                         break;
                 }
             }
